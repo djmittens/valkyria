e63f7563a59d24780d93dce80cf478cff9ed96a0
djmittens <djmittens@users.noreply.github.com>
2025-06-01 13:26:05 -0700

    Simplifying raising logics

--- src/common.h
+++ src/common.h
@@ -5,15 +5,11 @@
 #define UNUSED(x) ({ (void)x; })
 
 // NOLINTBEGIN(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
-#define VALK_RAISE(__msg, ...)                                              \
-  do {                                                                      \
-    size_t __len = snprintf(NULL, 0, "%s:%d:%s || %s\n", __FILE_NAME__,     \
-                            __LINE__, __FUNCTION__, (__msg));               \
-    char __fmt[__len + 1];                                                  \
-    snprintf(__fmt, __len + 1, "%s:%d:%s || %s\n", __FILE_NAME__, __LINE__, \
-             __FUNCTION__, (__msg));                                        \
-    fprintf(stderr, __fmt, ##__VA_ARGS__);                                  \
-    raise(SIGTRAP);                                                         \
+#define VALK_RAISE(__msg, ...)                                          \
+  do {                                                                  \
+    fprintf(stderr, "%s:%d:%s || " __msg "\n", __FILE_NAME__, __LINE__, \
+            __FUNCTION__, ##__VA_ARGS__);                               \
+    raise(SIGTRAP);                                                     \
   } while (0)
 // NOLINTEND(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
 
@@ -29,4 +25,3 @@ typedef enum {
   VALK_ERR_SSL_READ,
   VALK_ERR_SSL_ENCRYPT,
 } valk_err_e;
-
